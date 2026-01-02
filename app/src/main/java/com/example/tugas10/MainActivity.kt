26fd0ce7154be018b5d2c03d4631a9af016fde94
Ekoramadhan13 <enugraha411@gmail.com>
2026-01-02 16:35:58 +0700

    menamabhakan isi file PenyediaViewModel.kt

--- app/src/main/java/com/example/tugas10/MainActivity.kt
+++ app/src/main/java/com/example/tugas10/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tugas10.ui.theme.Tugas10Theme
+import com.example.tugas10.view.controllNavigasi.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Tugas10Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
