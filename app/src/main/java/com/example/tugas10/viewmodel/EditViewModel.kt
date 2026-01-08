9ba96e94f8ed80efaa6e445d1b908625774d2230
Ekoramadhan13 <enugraha411@gmail.com>
2026-01-08 17:35:17 +0700

    menambahkan isi file EditViewModel.kt

--- app/src/main/java/com/example/tugas10/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/tugas10/viewmodel/EditViewModel.kt
@@ -1,2 +1,37 @@
 package com.example.tugas10.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tugas10.modeldata.DetailSiswa
+import com.example.tugas10.modeldata.UIStateSiswa
+import com.example.tugas10.modeldata.toDataSiswa
+import com.example.tugas10.modeldata.toUiStateSiswa
+import com.example.tugas10.repositori.RepositorySiswa
+import com.example.tugas10.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
+
+}
