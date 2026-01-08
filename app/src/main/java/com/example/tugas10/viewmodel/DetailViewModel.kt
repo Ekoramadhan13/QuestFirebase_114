33721702c9ff8d781760c1a9f3b21d8e574c62a8
Ekoramadhan13 <enugraha411@gmail.com>
2026-01-08 17:31:41 +0700

    menambahkan code di file DetailViewModel.kt

--- app/src/main/java/com/example/tugas10/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/tugas10/viewmodel/DetailViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.tugas10.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tugas10.modeldata.Siswa
+import com.example.tugas10.repositori.RepositorySiswa
+import com.example.tugas10.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val siswa: Siswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+
