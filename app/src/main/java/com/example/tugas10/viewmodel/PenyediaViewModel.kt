2312fe34ebf9cd1c1cfb34d5323d1aa5f22519ea
Ekoramadhan13 <enugraha411@gmail.com>
2026-01-08 17:37:50 +0700

    Memperbaiki isi file di PenyediaViewModel.kt

--- app/src/main/java/com/example/tugas10/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/tugas10/viewmodel/PenyediaViewModel.kt
@@ -1,19 +1,40 @@
 package com.example.tugas10.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.tugas10.repositori.AplikasiDataSiswa
 
-fun CreationExtras.aplikasiDataSiswa():AplikasiDataSiswa = (
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
         this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
                 AplikasiDataSiswa
         )
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
-        initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
-        initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
